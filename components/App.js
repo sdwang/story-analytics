63d8c74cf0a1e853e549f6aa7c92003515a46b6d
Si Wang <siwang22@gmail.com>
2017-03-22 23:04:08 -0700

    (add) styles bundle

--- components/App.js
+++ components/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+const ipcRenderer = window.require('electron').ipcRenderer
+
+//Require for webpack to use as a module
+require('../styles/styles.css');
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
-        <div>
-          We are using Node.js {process.versions.node}
-          Chromium {process.versions.chrome}
-          and Electron {process.versions.electron}
-        </div>
       </div>
     );
   }
